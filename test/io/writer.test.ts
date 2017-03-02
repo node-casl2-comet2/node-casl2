1a14d4a77014e57572c71e1504ef51277d3ab5ee
Maxfield Walker <maxfield.walker@gmail.com>
2017-03-02 23:24:53 +0900

    modify create binary buffer test because createBinaryBuffer now takes array of 2-byte number for its argument

--- test/io/writer.test.ts
+++ test/io/writer.test.ts
@@ -5,15 +5,13 @@ import * as assert from "assert";
 
 suite("Writer test", () => {
     test("create binary buffer test", () => {
-        const buffer = Writer.createBinaryBuffer([0x1234, 0x5678, 0x12345678]);
-        assert.equal(buffer.byteLength, 8);
+        const buffer = Writer.createBinaryBuffer([0x1234, 0x5678]);
+
+        assert.equal(buffer.byteLength, 4);
+
         assert.equal(buffer.readUInt8(0), 0x12);
         assert.equal(buffer.readUInt8(1), 0x34);
         assert.equal(buffer.readUInt8(2), 0x56);
         assert.equal(buffer.readUInt8(3), 0x78);
-        assert.equal(buffer.readUInt8(4), 0x12);
-        assert.equal(buffer.readUInt8(5), 0x34);
-        assert.equal(buffer.readUInt8(6), 0x56);
-        assert.equal(buffer.readUInt8(7), 0x78);
     });
 });
